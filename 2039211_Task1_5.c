9b4478c60bedb3ea082b6151b43f5459b33409a9
Samir Husen <saw.mendes.meer@gmail.com>
2020-12-22 15:30:37 +0545

    Task 1 5

--- 2039211_Task1_5.c
+++ 2039211_Task1_5.c
@@ -1,1 +1,19 @@
-vvv
+/**********************************************************************************
+
+
+  The main function below show we can simulate the process of producing the C and B    values  
+
+
+***********************************************************************************/
+
+
+int main(int argc, char** argv)
+
+{
+	int A = 1, B = 2, C = 1, D = 4;
+	int B1 = A + C;
+	int C1 = C;
+
+	B = C1 + D;
+	C = B1 + D;
+}
